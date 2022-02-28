720362ee3cd6e403cbdf4aceb799658a84d8a5c2
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-02-28 18:21:21 -0300

    update

--- js/app.js
+++ js/app.js
@@ -7,17 +7,25 @@ function send() {
         firstName.classList.add('error')
         let errorMsg = document.getElementsByClassName('error-msg')[0]
         errorMsg.style.display = 'inline'
+    } else {
+        firstName.classList.remove('error')
     } if (lastName.value.length <= 0) {
         lastName.classList.add('error')
         let errorMsg = document.getElementsByClassName('error-msg')[1]
         errorMsg.style.display = 'inline'
+    } else {
+        lastName.classList.remove('error')
     } if (email.value.length <= 0) {
         email.classList.add('error')
         let errorMsg = document.getElementsByClassName('error-msg')[2]
         errorMsg.style.display = 'inline'
+    } else{
+        email.classList.remove('error')
     } if (password.value.length <= 0) {
         password.classList.add('error')
         let errorMsg = document.getElementsByClassName('error-msg')[3]
         errorMsg.style.display = 'inline'
+    } else {
+        password.classList.remove('error')
     }
 }
